46b55ce6ac38285778c875f591628249af6df39f
Heidi Wang <90928693+Heidiiwang@users.noreply.github.com>
2021-11-17 17:37:36 -0800

    Create aritziaregister.php

---
+++ aritziaregister.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "datalab3.bus.sfu.ca">
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Aritzia's Registration Page</title>
+    <link rel="stylesheet" href="AritziaStyleSheet.css" />
+	<h1><img id = "aritzia", 
+		src="https://www.aritzia.com/on/demandware.static/Sites-Aritzia_CA-Site/-/default/dw29c878d3/images/aritzia_skin/aritzia_logo.svg"> 
+	</h1>
+	<br><br>
+	<hr>
+	<br><br>
+	<h2 id= "customerlogintitle"> Registration </h2>
+	<br><br>
+	<hr>
+	<br>
+</head>
+<body>
+</body>
+</html>
