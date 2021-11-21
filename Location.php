67e6636e092fcc6334d8e8416a3ab26cee7e3dac
Scu21 <90928745+Scu21@users.noreply.github.com>
2021-11-21 12:01:22 -0800

    Update Location.php

--- Location.php
+++ Location.php
@@ -1,7 +1,17 @@
 <html> 
 <head>
-<Title> Location | Aritzia </Title>
-
+	<title>Aritzia's Location</title>
+    <link rel="stylesheet" href="AritziaStyleSheet.css?v=<?php echo time(); ?>">
+	<h1><img id = "aritzia", 
+		src="https://www.aritzia.com/on/demandware.static/Sites-Aritzia_CA-Site/-/default/dw29c878d3/images/aritzia_skin/aritzia_logo.svg"> 
+	</h1>
+	<br><br>
+	<hr>
+	<br><br>
+	<h2 id= "customerlogintitle"> Location </h2>
+	<br><br>
+	<hr>
+	<br>
 </head>
 
 <body>
