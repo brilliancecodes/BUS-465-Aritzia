6b937783a3f68631245b304ff5fc2e356cf5de34
Scu21 <90928745+Scu21@users.noreply.github.com>
2021-11-21 12:02:44 -0800

    Update Cart.php

--- Cart.php
+++ Cart.php
@@ -1,8 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
+	
+		<title>Cart</title>
+    <link rel="stylesheet" href="AritziaStyleSheet.css?v=<?php echo time(); ?>">
+	<h1><img id = "aritzia", 
+		src="https://www.aritzia.com/on/demandware.static/Sites-Aritzia_CA-Site/-/default/dw29c878d3/images/aritzia_skin/aritzia_logo.svg"> 
+	</h1>
+	<br><br>
+	<hr>
+	<br><br>
+	<h2 id= "customerlogintitle"> Cart </h2>
+	<br><br>
+	<hr>
+	<br>
 <link rel="stylesheet" href="AritziaStyleSheet.css"/>
-<title>Cart</title>
+
 <script type="text/javascript">
     function select()
     {
