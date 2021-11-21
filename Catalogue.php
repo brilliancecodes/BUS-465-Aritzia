f3edc413d7cba318628be4dd33f0f21833a71d8d
Scu21 <90928745+Scu21@users.noreply.github.com>
2021-11-21 12:04:36 -0800

    Update Catalogue.php

--- Catalogue.php
+++ Catalogue.php
@@ -1,8 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<title>Catalogue</title>
 <link rel="stylesheet" href="AritziaStyleSheet.css"/>
-<title>Catalogue</title>
+		
+    <link rel="stylesheet" href="AritziaStyleSheet.css?v=<?php echo time(); ?>">
+	<h1><img id = "aritzia", 
+		src="https://www.aritzia.com/on/demandware.static/Sites-Aritzia_CA-Site/-/default/dw29c878d3/images/aritzia_skin/aritzia_logo.svg"> 
+	</h1>
+	<br><br>
+	<hr>
+	<br><br>
+	<h2 id= "customerlogintitle"> Catalogue </h2>
+	<br><br>
+	<hr>
+	<br>
+	
+	
 </head>
 
 <body>
