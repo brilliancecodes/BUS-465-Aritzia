200290e22dffb2ea546ed8b4524ef404ca638292
Scu21 <90928745+Scu21@users.noreply.github.com>
2021-12-13 02:00:22 -0800

    Add files via upload
    Cancel will Cancel Order and update DB

---
+++ Cancel.php
@@ -0,0 +1,21 @@
+<?php
+$db= mysqli_connect("localhost", "hwa134", "Q968EhWeHbBFc74LfZtsdYXrG");
+if (!db) {die("Connection failed: ". mysqli_connect_error()); }
+mysqli_select_db($db, "AritziaDB");
+
+$sql = "UPDATE orders SET Status = 'Cancelled' WHERE email = '$email'
+order by ordernumber desc limit 1";
+
+if($db -> query($sql) === TRUE){
+	echo "ORDER has been Cancelled";
+
+}
+else {
+	echo "Error:" . $sql . "<br>" . $db -> error;
+
+}
+
+echo '<div class="submitbutton"><a href = "CatalogueList.php">
+<input type="submit" name="backtocatalogue" value="Back to Catalogue" id="submit"></a>';
+
+?>
